ad5025a50c5b7d61abe91358b93b5554933dceba
NikSigma <mykolakotkov1@gmail.com>
2025-06-05 15:26:15 +0500

    Create, done Task 2

---
+++ task2.py
@@ -0,0 +1,4 @@
+print("1 - Morning routine:", "wake up at 6:30, breakfast, go kayaking,  ")
+print("2 - Lunch time:", "relax, play on guitar, lunch, ")
+print("3 - Evening routine:", "relax, go outside, dinner, ")  
+print("4 - Night:", "shower, read, go to sleep at 23:30.")
